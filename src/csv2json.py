3e0ed9e874c39d64f972de052a1b61649573e2da
Sparks-Lu <sparkslu@gmail.com>
2019-12-12 11:49:22 +0800

    Added first workable version.

---
+++ src/csv2json.py
@@ -0,0 +1,66 @@
+import os
+import sys
+import collections
+
+
+class CSV2JSON(object):
+    def __init__(self):
+        pass
+
+
+    def __del__(self):
+        pass
+
+
+    def convert(self, fn_csv, fn_json=None):
+        lines = []
+        with open(fn_csv, 'r') as f:
+            lines = f.readlines()
+            f.close()
+        if len(lines) == 0:
+            return
+
+        # parse csv
+        contents = collections.OrderedDict()
+        key = None
+        for line in lines:
+            line = line.strip()
+            if line.find('\t') >= 0:
+                key, value = line.split('\t')
+                contents[key] = value
+            elif key is not None:
+                # append to last key
+                contents[key] = contents[key] + '\t' + line
+
+        # write json
+        if fn_json is None:
+            dn = os.path.dirname(fn_csv)
+            if dn == '':
+                dn = '.'
+            fn_json = '{}/{}.json'.format(dn, os.path.basename(fn_csv))
+        with open(fn_json, 'w') as f:
+            for k in contents.keys():
+                line = '["'
+                line += k
+                line += '"'
+                values = contents[k].split('\t')
+                for v in values:
+                    line += ',"{}"'.format(v)
+                line += '],\n'
+                f.write(line)
+            f.close()
+            print('JSON was written as {} successfully.'.format(fn_json))
+
+
+def main():
+    cj = CSV2JSON()
+    if len(sys.argv) < 2:
+        print('Usage: python {} <csv_filename>'.format(__file__))
+        return
+    fn_input = sys.argv[1]
+    cj.convert(fn_input)
+
+
+if __name__ == '__main__':
+    main()
+
